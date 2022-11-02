430b57be9d2cb6a6c8aaefd3dcf0d6ebf1200609
Trond Magnus Sevre <76692689+trondsevre@users.noreply.github.com>
2022-11-02 10:11:29 +0100

    Add samtykke (#7)
    * Add samtykke
    
    * Update gradle and dependencies

---
+++ src/main/java/no/fintlabs/custom/samtykke/SamtykkePublisher.java
@@ -0,0 +1,38 @@
+package no.fintlabs.custom.samtykke;
+
+import lombok.extern.slf4j.Slf4j;
+import no.fint.model.resource.personvern.samtykke.SamtykkeResource;
+import no.fintlabs.adapter.AdapterProperties;
+import no.fintlabs.adapter.ResourcePublisher;
+import no.fintlabs.adapter.ResourceRepository;
+import no.fintlabs.adapter.models.AdapterCapability;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SamtykkePublisher extends ResourcePublisher<SamtykkeResource, ResourceRepository<SamtykkeResource>> {
+
+    public SamtykkePublisher(SamtykkeRepository repository, AdapterProperties adapterProperties) {
+        super(repository, adapterProperties);
+    }
+
+    @Override
+    @Scheduled(initialDelayString = "10000", fixedRateString = "#{@adapterProperties.getFullSyncIntervalMs('samtykke')}")
+    public void doFullSync() {
+        log.info("Start full sync for resource {}", getCapability().getEntityUri());
+        submit(repository.getResources());
+    }
+
+    @Override
+    @Scheduled(initialDelayString = "60000", fixedRateString = "30000")
+    public void doDeltaSync() {
+        log.info("Start delta sync for resource {}", getCapability().getEntityUri());
+        submit(repository.getUpdatedResources());
+    }
+
+    @Override
+    protected AdapterCapability getCapability() {
+        return adapterProperties.getCapabilityByResource("samtykke");
+    }
+}
